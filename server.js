7ed6c4e46603c8dc4971599e990b7c285e997bde
Ingrid <ingridsu@yahoo.com>
2019-09-26 15:16:48 -0700

    update to smart proxy

--- server.js
+++ server.js
@@ -11,8 +11,15 @@ const PORT = 3000;
 // app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(cors());
-app.use(express.static(path.resolve(__dirname, './public')));
+app.use(express.static(path.resolve(__dirname, 'public')));
 app.use(`/listing/:id`, express.static('public'))
+
+
+app.get('/api/rooms/:id/reviews', (req, res)=>{
+  let id = req.params.id;
+  res.redirect(`http://localhost:3003/api/rooms/${id}/reviews`)
+})
+
 
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`);
